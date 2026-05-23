6de909ca76c77ef7eeef861d7949ff96b2b78d44
shashwat3050 <shashwat3050@gmail.com>
2026-05-23 07:50:46 +0530

    updated

--- DSA/src/May_22/hollow_tri.java
+++ DSA/src/May_22/hollow_tri.java
@@ -2,8 +2,19 @@ package May_22;// requirement is double gap
 
 public class hollow_tri {
     public static void main(String[] args){
-        for(int i=0; i<5; i++) {
-
+        for(int i=0; i<5; i++){
+            for(int j=0; j<5-i; j++){
+                System.out.print(" ");
+            }
+            //System.out.println();
+            for(int k=0; k<=i; k++){
+                if(k==i || k==0 || i==5-1){
+                    System.out.print("* ");
+                } else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
         }
     }
 }
