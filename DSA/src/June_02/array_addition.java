34da70ac7df12edd6d9db5fe8a256606e6088d00
shashwat3050 <shashwat3050@gmail.com>
2026-06-03 17:21:21 +0530

    commit

--- DSA/src/June_02/array_addition.java
+++ DSA/src/June_02/array_addition.java
@@ -3,55 +3,49 @@ package June_02;
 import java.util.Arrays;
 
 public class array_addition {
-    static int[] calSum(int a[], int b[], int n, int m) {
+    static void sum(int arr1[], int arr2[], int n, int m) {
         // Result array size can be max(n,m) + 1
-        int[] res = new int[Math.max(n, m) + 1];
+        int[] res = new int[Math.max(n, m) +1];
 
-        int i = n - 1;
-        int j = m - 1;
-        int k = res.length - 1;
-        int carry = 0;
+        int arr1_len = n -1;
+        int arr2_len = m -1;
+        int res_len = res.length -1;
+        int carry =0;
 
-        // Add digits from back
-        while(i >= 0 || j >= 0 || carry > 0) {
-            int sum = carry;
-            if(i >= 0) {
-                sum = sum + a[i];
-                i--;
+        while(arr1_len >=0 || arr2_len >=0 || carry >0) { //adding digits from the back one at a time from both the arrays and
+            int sum = carry;                              //checking if the carry was generated
+            if(arr1_len >=0) {
+                sum = sum + arr1[arr1_len];
+                arr1_len--;
             }
-
-            if(j >= 0) {
-                sum = sum + b[j];
-                j--;
+            if(arr2_len >=0) {
+                sum = sum + arr2[arr2_len];
+                arr2_len--;
             }
-            res[k] = sum % 10;
-            carry = sum / 10;
-            k--;
+            res[res_len] = sum%10;
+            carry = sum/10;
+            res_len--;
         }
 
         //remove trailing 0
         // Remove leading zero if not needed
-        int sz = res.length;
+        int len = res.length;
         if(res[0] == 0) {
+            int[] ans = new int[len - 1];
 
-            int[] ans = new int[sz - 1];
-
-            for(int x = 1; x < sz; x++) {
+            for(int x = 1; x < len; x++) {
                 ans[x - 1] = res[x];
             }
-
-            return ans;
+            System.out.println(Arrays.toString(ans));
         }
-        return res;
-
+        System.out.println(Arrays.toString(res));
     }
+
     public static void main(String[] args) {
-        int[] arr1 = {1,2,3};
-        int[] arr2 = {3,2,1};
+        int[] nums1 = {4,4,4};
+        int[] nums2 = {5,5,6};
         int n = 3 , m = 3;
 
-        int[] res = calSum(arr1, arr2, n, m);
-
-        System.out.println(Arrays.toString(res));
+        sum(nums1, nums2, n, m);
     }
 }
